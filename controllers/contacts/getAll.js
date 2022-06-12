d1b0a4d6af57e19e15b2597e62b1ab57eeb36805
albert95gt <albitakacs@gmail.com>
2022-06-12 11:13:23 +0200

    fixed filter at contact operation

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,18 +2,26 @@ const { Contact } = require('../../models');
 
 const getAll = async (req, res, next) => {
   try {
-    const { page = 1, limit = 10, favorite = false } = req.query;
+    const { page = 1, limit = 10, favorite } = req.query;
     const { _id } = req.user;
 
     const skipped = (page - 1) * limit;
     const skip = skipped < 0 ? 0 : skipped;
 
-    const contacts = await Contact.find({ owner: _id, favorite }, '', {
-      skip,
-      limit: Number(limit),
-    }).populate('owner', '_id email subscription');
+    if (favorite) {
+      const contacts = await Contact.find({ owner: _id, favorite }, '', {
+        skip,
+        limit: Number(limit),
+      }).populate('owner', '_id email subscription');
 
-    res.json(contacts);
+      res.json(contacts);
+    } else {
+      const contacts = await Contact.find({ owner: _id }, '', {
+        skip,
+        limit: Number(limit),
+      }).populate('owner', '_id email subscription');
+      res.json(contacts);
+    }
   } catch (error) {
     next(error);
   }
