0f94f0b984994dc49bb7a3733e6f41d5e616002f
albert95gt <albitakacs@gmail.com>
2022-07-03 09:40:28 +0200

    fixed base-url

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const TEMP_DIR = path.join(__dirname, '../', 'temp');
 const AVATARS = 'avatars';
-const BASE_URL = 'http://localhost:3000/api';
+const BASE_URL = 'https://mighty-spire-59228.herokuapp.com/api';
 
 module.exports = {
   TEMP_DIR,
